9ce43630300717baccb578505be4d52edefcab6b
Staneesh <blkreasons@outlook.com>
2020-06-13 10:41:41 +0200

    structs instead of raw values

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
-fn area(width: u32, height: u32) -> u32
+struct Rectangle
 {
-    width * height
+    width: u32,
+    height: u32,
+}
+
+fn area(rectangle: &Rectangle) -> u32
+{
+    rectangle.width * rectangle.height
 }
 
 fn main() {
-    let width = 30;
-    let height = 30;
+    
+    let rect = Rectangle
+    {
+        width: 30,
+        height: 30,
+    };
 
     println!(
         "The area of a rectangle is {} square pixels.",
-        area(width, height)
+        area(&rect)
         );
 }
 
