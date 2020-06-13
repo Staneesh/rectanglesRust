9b437e4dd6b4e42defe7f7a805c90948f6a2ef80
Staneesh <blkreasons@outlook.com>
2020-06-13 11:07:01 +0200

    can_hold method

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,39 @@ impl Rectangle
     {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool
+    {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
     
-    let rect = Rectangle
+    let rect1 = Rectangle
     {
         width: 30,
         height: 30,
     };
 
+    let rect2 = Rectangle
+    {
+        width: 20,
+        height: 10,
+    };
+
+    let rect3 = Rectangle
+    {
+        width: 40,
+        height: 5,
+    };
+
     println!(
         "The area of a rectangle {:#?} is {} square pixels.",
-        rect, rect.area() 
+        rect1, rect1.area() 
         );
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
