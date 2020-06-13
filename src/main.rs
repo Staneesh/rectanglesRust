711ca0fd8ba60ef503132234f1aaad698854cb74
Staneesh <blkreasons@outlook.com>
2020-06-13 10:36:33 +0200

    no structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+fn area(width: u32, height: u32) -> u32
+{
+    width * height
 }
+
+fn main() {
+    let width = 30;
+    let height = 30;
+
+    println!(
+        "The area of a rectangle is {} square pixels.",
+        area(width, height)
+        );
+}
+
